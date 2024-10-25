f11dc76e1308e8690fb34b4ed8b8b1d4eb35ff6c
valitovgaziz <valitovgaziz@gmail.com>
2024-10-25 20:52:54 +0500

    count all that what by resiving two args

--- NumberOfSymbols.js
+++ NumberOfSymbols.js
@@ -8,4 +8,21 @@ function countBs(str) {
     return numbersOfB
 }
 
+function countChar(str, adf) {
+    let charMap = new Map()
+    for (let i = 0; i < adf.length; i++) {
+        charMap.set(adf[i], 0)
+        for (let j = 0; j < str.length; j++) {
+            if (str[j] === adf[i]) {
+                number = charMap.get(adf[i]) + 1
+                charMap.set(adf[i], number)
+            }
+        }
+    }
+    console.log(charMap)
+}
+
+console.log("Function count B")
 console.log(countBs("BBB"))
+console.log("Function count Chars")
+console.log(countChar("ABCCCCc", "ABC"))
