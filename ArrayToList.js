19e2c1d54e7d5929388556d773d7ee36ab909800
valitovgaziz <valitovgaziz@gmail.com>
2024-10-28 00:21:34 +0500

    Reduce

---
+++ ArrayToList.js
@@ -0,0 +1,9 @@
+function arrayToList(arr) {
+    for (let item in arr) {
+        console.log(item)
+    }
+}
+
+let arr = [1,2,3,4,5]
+
+arrayToList(arr)
---
+++ reduce.js
@@ -0,0 +1,12 @@
+function reduce() {
+
+}
+
+let arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9,10]
+]
+let result = []
+let someNewArr = arr.reduce((resArr, arr) => resArr.concat(arr))
+console.log(someNewArr)
