2dc326f64cdc16562b8a9c12ceb1ae2bce3de042
valitovgaziz <valitovgaziz@gmail.com>
2024-10-31 21:58:35 +0500

    SCRIPTS is not in this lessons

---
+++ MainDairection.js
@@ -0,0 +1,20 @@
+let directions = ["ltr", "rtl", "ttb"]
+
+
+let strLetters = "aaflds"
+
+function MainDairection(str) {
+    let arr = []
+    for (let i = 0; i < arr.length; i++) {
+        console.log(characterScript(str[i]))
+    }
+}
+
+console.log(characterScript("A"))
+MainDairection(strLetters)
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        
+    }
+}
