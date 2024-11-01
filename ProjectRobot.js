8a02a9ce7d5a01034c054e08f5dd5b31a3f2a29c
valitovgaziz <valitovgaziz@gmail.com>
2024-11-01 22:29:01 +0500

    Random choose of way for robot function

--- ProjectRobot.js
+++ ProjectRobot.js
@@ -52,12 +52,6 @@ let first = new VillageState(
     [{place: "Почта", address: "Дом Алисы"}]
 );
 
-let next = first.move("Дом Алисы");
-console.log(next.place);
-console.log(next.parcels);
-console.log(first.place);
-console.log(first.parcels);
-
 function runRobot(state, robot, memory) {
     for (let turn = 0;; turn++) {
         console.log(`Выполненно за ${turn} ходов`)
@@ -68,3 +62,51 @@ function runRobot(state, robot, memory) {
     memory = action.memory;
     console.log(`Переход в направлении ${action.direction}`);
 }
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Почта", parcels)
+}
+
+const mailRoute = [
+    "Дом Алисы", "Сарай", "Дом Алисы", "Дом Боба", "Ратуша", "Дом Дарии", "Дом Дарии",
+    "Дом Эрни", "Дом Греты", "Магазин", "Дом Греты", "Ферма", "Рынок", "Почта"
+];
+
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+}
+
+function findRout(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
